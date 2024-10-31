63ca57d8c236eb4ca7df4b64b0bd7593f5ed7086
JulkaUlka <y.glushko@outlook.com>
2024-10-31 15:34:46 +0200

    feat: add basic Express server setup

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-import OpenAI from "openai";
-import TelegramBot from "node-telegram-bot-api";
-import dotenv from "dotenv";
+import OpenAI from 'openai';
+import TelegramBot from 'node-telegram-bot-api';
+import dotenv from 'dotenv';
+import express from 'express';
+
 dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`serve at ${port}`);
+});
 
 //OpenAi
 const info = process.env.INFO;
-const error = "Ask the answer to this question directly from Yulia😉";
+const error = 'Ask the answer to this question directly from Yulia😉';
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
@@ -14,14 +22,14 @@ async function main(question) {
   const completion = await openai.chat.completions.create({
     messages: [
       {
-        role: "system",
-        content: `You are a personal, friendly assistant who knows everything about Yuliia Hlushko. You talk to HR about Yuliia. Your goal is to help Yuliia pass her interview by providing answers based only on the information provided below. It is important to respond in a way that pleases her and to use the language of the user's question. If there is no information available to answer a specific question, please write "${error}" in the language of the user's question or give your funny answer.
+        role: 'system',
+        content: `You are a personal, friendly assistant who knows everything about Yuliia Hlushko. You talk to HR about Yuliia, not to Yuliia. Your goal is to help Yuliia pass her interview by providing answers based only on the information provided below. It is important to respond in a way that pleases her and to use the language of the user's question. If there is no information available to answer a specific question, please write "${error}" in the language of the user's question or give your funny answer.
 
         Info about Yuliia:${info};`,
       },
-      { role: "user", content: `my question:${question}` },
+      { role: 'user', content: `my question:${question}` },
     ],
-    model: "gpt-4o-mini-2024-07-18",
+    model: 'gpt-4o-mini-2024-07-18',
   });
 
   return completion.choices[0].message.content;
@@ -31,22 +39,22 @@ async function main(question) {
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on("message", async (msg) => {
+bot.on('message', async msg => {
   const chatId = msg.chat.id;
   const messageText = msg.text;
 
-  bot.sendChatAction(chatId, "typing");
+  bot.sendChatAction(chatId, 'typing');
 
   let answer;
 
   try {
     answer = await main(messageText);
   } catch (error) {
-    console.error("Error:", error);
-    answer = "Sorry, something went wrong. Try again later 😩";
+    console.error('Error:', error);
+    answer = 'Sorry, something went wrong. Try again later 😩';
   }
 
-  if (messageText === "/start") {
+  if (messageText === '/start') {
     bot.sendMessage(
       chatId,
       "Hello, I am Yuliia's personal assistant. I can guide you through her CV. Please write your question 🙂"
@@ -70,6 +78,6 @@ async function sendToSecondBot(messageText, answer) {
       `User: ${messageText}\nBot: ${answer}`
     );
   } catch (error) {
-    console.error("Error:", error);
+    console.error('Error:', error);
   }
 }
